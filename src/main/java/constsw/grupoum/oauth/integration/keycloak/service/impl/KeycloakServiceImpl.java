c484efe06aaec9eec8eee4a720b781f681033f93
jhonatapers <jhonatapers@gmail.com>
2023-04-01 17:26:30 -0300

    camadas base

---
+++ src/main/java/constsw/grupoum/oauth/integration/keycloak/service/impl/KeycloakServiceImpl.java
@@ -0,0 +1,7 @@
+package constsw.grupoum.oauth.integration.keycloak.service.impl;
+
+import constsw.grupoum.oauth.integration.keycloak.service.KeycloakService;
+
+public class KeycloakServiceImpl implements KeycloakService {
+
+}
