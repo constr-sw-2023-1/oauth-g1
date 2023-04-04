0b32fda65c358cb963cd093617b126f2eccd8fa4
jhonatapers <jhonatapers@gmail.com>
2023-04-04 17:45:43 -0300

    remocao @JsonProperty("grant_type")

--- src/main/java/constsw/grupoum/oauth/integration/keycloak/record/RequestToken.java
+++ src/main/java/constsw/grupoum/oauth/integration/keycloak/record/RequestToken.java
@@ -1,12 +1,9 @@
 package constsw.grupoum.oauth.integration.keycloak.record;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record RequestToken(String realm,
-        String clientId,
-        String username,
-        String password,
-        @JsonProperty("grant_type")
-        String grantType,
-        String clientSecret) {
+                String clientId,
+                String username,
+                String password,
+                String grantType,
+                String clientSecret) {
 }
