41db3e53cf482fee0bc2314812a1a519510d0bd9
jhonatapers <jhonatapers@gmail.com>
2023-04-04 17:44:08 -0300

    endpoints token + userinfo

--- src/main/java/constsw/grupoum/oauth/integration/keycloak/service/KeycloakService.java
+++ src/main/java/constsw/grupoum/oauth/integration/keycloak/service/KeycloakService.java
@@ -1,5 +1,15 @@
 package constsw.grupoum.oauth.integration.keycloak.service;
 
+import constsw.grupoum.oauth.integration.keycloak.exception.KeycloakException;
+import constsw.grupoum.oauth.integration.keycloak.record.RequestToken;
+import constsw.grupoum.oauth.integration.keycloak.record.RequestUserInfo;
+import constsw.grupoum.oauth.integration.keycloak.record.Token;
+import constsw.grupoum.oauth.integration.keycloak.record.UserInfo;
+
 public interface KeycloakService {
-    
+
+    Token token(RequestToken requestToken) throws KeycloakException;
+
+    UserInfo userInfo(RequestUserInfo requestUserInfo) throws KeycloakException;
+
 }
