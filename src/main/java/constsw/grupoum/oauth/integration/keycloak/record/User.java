fe2d1788fe986e9385a15ddbee527f6a630c4ddd
micaela-lechmann <lechmannmicaela@gmail.com>
2023-04-10 20:47:26 -0300

    add enabled query param to get all users

--- src/main/java/constsw/grupoum/oauth/integration/keycloak/record/User.java
+++ src/main/java/constsw/grupoum/oauth/integration/keycloak/record/User.java
@@ -1,11 +1,8 @@
 package constsw.grupoum.oauth.integration.keycloak.record;
 
 public record User(String id,
-        String createdTimestamp,
-        String username,
-        Boolean enabled,
-        Boolean emailVerified,
-        String firstName,
-        String lastName,
-        String email) {
+                String username,
+                Boolean enabled,
+                String firstName,
+                String lastName) {
 }
