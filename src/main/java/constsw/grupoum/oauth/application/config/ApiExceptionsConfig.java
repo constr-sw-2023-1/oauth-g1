5108e0d23de945bb481defd8b5d31c91adfbd102
jhonatapers <jhonatapers@gmail.com>
2023-04-11 13:15:02 -0300

    adiciona exceptions criadas

--- src/main/java/constsw/grupoum/oauth/application/config/ApiExceptionsConfig.java
+++ src/main/java/constsw/grupoum/oauth/application/config/ApiExceptionsConfig.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import constsw.grupoum.oauth.application.exception.ApiException;
+import constsw.grupoum.oauth.application.exception.BadRequestException;
+import constsw.grupoum.oauth.application.exception.ForbiddenException;
+import constsw.grupoum.oauth.application.exception.InternalServerErrorException;
+import constsw.grupoum.oauth.application.exception.InvalidCredentialsException;
+import constsw.grupoum.oauth.application.exception.NotFoundException;
 import constsw.grupoum.oauth.application.exception.UnauthorizedException;
 
 @Configuration
@@ -14,7 +19,12 @@ public class ApiExceptionsConfig {
 
     @Bean
     public Collection<ApiException> estrategias() {
-        return Arrays.asList(new UnauthorizedException());
+        return Arrays.asList(new UnauthorizedException(),
+                new BadRequestException(),
+                new InternalServerErrorException(),
+                new ForbiddenException(),
+                new NotFoundException(),
+                new InvalidCredentialsException());
     }
 
 }
