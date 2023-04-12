879d11545cd9c686378395010b157e83f5185f51
jhonatapers <jhonatapers@gmail.com>
2023-04-12 00:58:49 -0300

    senha no create

--- src/main/java/constsw/grupoum/oauth/application/record/RequestNewUser.java
+++ src/main/java/constsw/grupoum/oauth/application/record/RequestNewUser.java
@@ -2,9 +2,9 @@ package constsw.grupoum.oauth.application.record;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record RequestNewUser(
-        @JsonProperty("username") String username,
-        @JsonProperty("first_name") String firstName,
-        @JsonProperty("last_name") String lastName) {
+public record RequestNewUser(@JsonProperty("username") String username,
+                @JsonProperty("first_name") String firstName,
+                @JsonProperty("last_name") String lastName,
+                @JsonProperty("password") String password) {
 
 }
