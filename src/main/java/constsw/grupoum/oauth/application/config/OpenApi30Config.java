a3d299e76308094309b62dcb493406d1990b8bc3
jhonatapers <jhonatapers@gmail.com>
2023-04-13 18:07:09 -0300

    mudanca de titulo e adicionado external docs

--- src/main/java/constsw/grupoum/oauth/application/config/OpenApi30Config.java
+++ src/main/java/constsw/grupoum/oauth/application/config/OpenApi30Config.java
@@ -2,13 +2,14 @@ package constsw.grupoum.oauth.application.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "OAuth", version = "v1"), externalDocs = @ExternalDocumentation(url = "https://github.com/constr-sw-2023-1/oauth-g1/wiki"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class OpenApi30Config {
 
